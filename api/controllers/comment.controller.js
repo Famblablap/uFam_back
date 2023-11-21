0614919be843cb5e65c8155c4a5d8cde26a09236
caarolbrown <carolinamorenofdez@gmail.com>
2023-11-21 16:14:46 +0000

    controllers y router de comments (photo y video) hecho

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -2,22 +2,23 @@ const Comment_Photos = require('../models/comment_photos.model')
 const Comment_Videos = require('../models/comment_videos.model')
 const User = require('../models/user.model')
 const Photo = require('../models/photo.model')
+const Video = require('../models/video.model')
 
-async function createCommentPhoto (req, res){
+async function createCommentPhoto(req, res) {
     try {
         const comment = await Comment_Photos.create(req.body)
         const user = await User.findByPk(res.locals.user.id)
         if (!user) { res.status(404).send('User not found') }
-        const photo = await Photo.findByPk(req.params.photpId)
+        const photo = await Photo.findByPk(req.params.photoId)
         await comment.setPhoto(photo)
         await comment.setUser(user)
-        return res.status(200).json(comment) 
+        return res.status(200).json(comment)
     } catch (error) {
         return res.status(500).send(error.message)
     }
 }
 
-async function getCommentsPhoto (req, res){
+async function getCommentsPhoto(req, res) {
     try {
         const commentPhoto = await Comment_Photos.findAll({
             where: {
@@ -31,20 +32,20 @@ async function getCommentsPhoto (req, res){
     }
 }
 
-async function deleteCommentPhoto(req, res){
+async function deleteCommentPhoto(req, res) {
     try {
         const commentPhoto = await Comment_Photos.destroy({
             where: {
                 id: req.params.id
             }
         })
-        res.status(500).json ({ text: 'Comment photo removed', commentPhoto: commentPhoto})
+        res.status(500).json({ text: 'Comment photo removed', commentPhoto: commentPhoto })
     } catch (error) {
         return res.status(500).send(error.message)
     }
 }
 
-async function createCommentVideo (req, res){
+async function createCommentVideo(req, res) {
     try {
         const comment = await Comment_Videos.create(req.body)
         const user = await User.findByPk(res.locals.user.id)
@@ -52,7 +53,34 @@ async function createCommentVideo (req, res){
         const video = await Video.findByPk(req.params.videoId)
         await comment.setVideo(video)
         await comment.setUser(user)
-        return res.status(200).json(comment) 
+        return res.status(200).json(comment)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+async function getCommentsVideo(req, res) {
+    try {
+        const commentVideo = await Comment_Videos.findAll({
+            where: {
+                videoId: req.params.id
+            }
+        })
+        if (!commentVideo) { res.status(404).send('Comment video not found') }
+        return res.status(200).json(commentVideo)
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+async function deleteCommentVideo(req, res) {
+    try {
+        const commentVideo = await Comment_Video.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        res.status(500).json({ text: 'Comment video removed', commentVideo: commentVideo })
     } catch (error) {
         return res.status(500).send(error.message)
     }
@@ -62,5 +90,8 @@ async function createCommentVideo (req, res){
 module.exports = {
     createCommentPhoto,
     getCommentsPhoto,
-    deleteCommentPhoto
+    deleteCommentPhoto,
+    createCommentVideo,
+    getCommentsVideo,
+    deleteCommentVideo
 }
--- api/routes/comment.router.js
+++ api/routes/comment.router.js
@@ -1,9 +1,13 @@
-const { getCommentsPhoto, createCommentPhoto, deleteCommentPhoto } = require('../controllers/comment.controller')
+const { getCommentsPhoto, getCommentsVideo, createCommentPhoto, createCommentVideo, deleteCommentPhoto, deleteCommentVideo } = require('../controllers/comment.controller')
 
 const router = require('express').Router()
 
 router.get('/commentPhoto', getCommentsPhoto)
+router.get('/commentVideo', getCommentsVideo)
 router.post('/commentPhoto/:photoId', createCommentPhoto)
+router.post('/commentVideo/:videoId', createCommentVideo)
 router.delete('/commentPhoto/:photoId', deleteCommentPhoto)
+router.delete('/commentVideo/:videoId', deleteCommentVideo)
+
 
 module.exports = router
