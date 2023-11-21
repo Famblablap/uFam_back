a337efca733b2ab57dbb776e0a10ce1a6f09c2c1
caarolbrown <carolinamorenofdez@gmail.com>
2023-11-21 13:05:33 +0000

    actualizados los getOne con findbyPk

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ async function connectDB() {
 }
 
 function launchServer() {
-  const app = express()
-    .use(cors())
-    .use(morgan("dev"))
-    .use(express.json())
-    .use("/api", require("./api/routes/index.js"))
-    .listen(process.env.PORT, () => {
-      console.log("Express server listening on port" + process.env.PORT);
-    });
+    const app = express()
+        .use(cors())
+        .use(morgan('dev'))
+        .use(express.json())
+        .use('/api', require('./api/routes/index'))
+        .listen(process.env.PORT, () => {
+            console.log("Express server listening on port " + process.env.PORT)
+        })
 }
 
 async function startAPI() {
