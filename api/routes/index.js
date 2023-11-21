4a69d2ee80f5e62d0595585aaa49a996ad52a40d
Dokho <palbabyshark@gmail.com>
2023-11-21 16:27:29 +0000

    controllersj done

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,6 +6,8 @@ const blogRouter = require('./blog.router')
 const likeRouter = require('./like.router')
 const photoRouter = require('./photo.router')
 const commentRouter = require('./comment.router')
+const familyRouter = require("./family.router")
+const notificationRouter = require("./notification.router")
 // const messageRouter = require('./message.router')
 
 router.use('/auth', authRouter)
@@ -14,6 +16,9 @@ router.use('/blog', blogRouter)
 router.use('/like', likeRouter)
 router.use('/photo', photoRouter)
 router.use('/comments', commentRouter)
+router.use("/families", familyRouter)
+router.use("/notifications", notificationRouter)
+
 
 // router.use('/messages', messageRouter)
 
