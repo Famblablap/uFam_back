80252bc47c52a7de1d23fcfba5db628d518101c1
Dokho <palbabyshark@gmail.com>
2023-11-22 17:00:17 +0000

    in progress

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -59,43 +59,12 @@ async function getFamProfile(req, res) {
 
 async function createUser(req, res) {
   try {
-    // Extracting user details and family info from the request body
-    //In this function, userDetails contain the master user's personal details, familyName is the name of the family being created, and memberEmails is an array of emails of family members to be added.
-    const { userDetails, familyName, memberEmails } = req.body
-
-    // Create the master user
-    const user = await User.create(userDetails)
-
-    // Create a family and link the master user to it
-    const family = await Family.create({ family_name: familyName })
-    await family.addUser(user);
-
-    // Process the family member emails
-    for (const email of memberEmails) {
-      let member = await User.findOne({ where: { email } })
-
-      // If user exists, link them to the family
-      if (member) {
-        await family.addUser(member)
-      } else {
-        // Optionally handle case where user does not exist (e.g., send an invitation)
-      }
-    }
-
-    return res.status(200).send({ user, family })
+    const user = await User.create(req.body);
+    return res.status(200).send(user);
   } catch (error) {
-    return res.status(500).send(error.message)
+    return res.status(500).send(error.message);
   }
 } 
-
-// async function createUser(req, res) {
-//   try {
-//     const user = await User.create(req.body);
-//     return res.status(200).send(user);
-//   } catch (error) {
-//     return res.status(500).send(error.message);
-//   }
-// }
 
 async function updateUser(req, res) {
   try {
