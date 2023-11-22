9577517c483862d4318d525fc56f03884a49493b
Dokho <palbabyshark@gmail.com>
2023-11-22 14:19:43 +0000

    verified in progress

--- api/controllers/verified_email.controller.js
+++ api/controllers/verified_email.controller.js
@@ -1,49 +1,49 @@
 const VerifiedEmail = require('../models/verified_email.model')
 
-async function getAllVideos(req, res) {
+async function getAllVerifiedEmails(req, res) {
     try {
-        const videos = await Video.findAll()
-        return res.status(200).json(videos)
+        const verified_emails = await VerifiedEmail.findAll()
+        return res.status(200).json(verified_emails)
     } catch (error) {
         return res.status(500).send(error.message)
     }
 }
 
-async function getOneVideo(req, res) {
+async function getOneVerifiedEmail(req, res) {
     try {
-        const video = await Video.findByPk(req.params.id)
-        if (!video) { res.status(500).send('Video not found') }
-        return res.status(200).json(video)
+        const verified_email = await VerifiedEmail.findByPk(req.params.id)
+        if (!verified_email) { res.status(500).send('Verified Email not found') }
+        return res.status(200).json(verified_email)
     } catch (error) {
         res.status(500).send(error.message)
     }
 }
 
-async function createVideo(req, res) {
+async function createVerifiedEmail(req, res) {
     try {
-        const video = await Video.create(req.body)
-        return res.status(200).json(video)
+        const verified_email = await VerifiedEmail.create(req.body)
+        return res.status(200).json(verified_email)
     } catch (error) {
         return res.status(500).send(error.message)
     }
 }
 
-async function deleteVideo(req, res) {
+async function deleteVerifiedEmail(req, res) {
     try {
-        const video = await Video.destroy({
+        const verified_email = await VerifiedEmail.destroy({
             where: {
                 id: req.params.id
             }
         })
-        res.status(500).json({ text: 'Video removed', video: video })
+        res.status(500).json({ text: 'Verified Email removed', verified_email: verified_email })
     } catch (error) {
         return res.status(500).send(error.message)
     }
 }
 
 module.exports = {
-    getAllVideos,
-    getOneVideo,
-    createVideo,
-    deleteVideo
+    getAllVerifiedEmails,
+    getOneVerifiedEmail,
+    createVerifiedEmail,
+    deleteVerifiedEmail
 }
