5dad5a05bcd8cdbaabd7805d8e2892eaede544ee
caarolbrown <carolinamorenofdez@gmail.com>
2023-11-24 15:50:34 +0000

    createPhoto asigando userId done

--- api/controllers/photo.controller.js
+++ api/controllers/photo.controller.js
@@ -16,8 +16,11 @@ async function getAllPhotos(req, res) {
 async function getOnePhoto(req, res) {
   try {
     const photo = await Photo.findByPk(req.params.id)
-    if (!photo) { res.status(500).send('Photo not found') }
-    return res.status(200).json(photo)
+    if (photo) {
+      return res.status(200).json(photo)
+    } else {
+      return res.status(404).json("Phoho not found")
+    }
   } catch (error) {
     res.status(500).send(error.message)
   }
@@ -81,6 +84,8 @@ async function getAllFamPhotos(req, res) {
 async function createPhoto(req, res) {
   try {
     const photo = await Photo.create(req.body)
+    const user = await User.findByPk(res.locals.user.id)
+    await user.addPhoto(photo)
     return res.status(200).json(photo)
   } catch (error) {
     return res.status(500).send(error.message)
