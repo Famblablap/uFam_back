e2672739128915fcd85a66fcbe7e0077253f7d9e
Dokho <palbabyshark@gmail.com>
2023-11-29 12:57:12 +0000

    photo upload ok

--- api/controllers/photo.controller.js
+++ api/controllers/photo.controller.js
@@ -84,12 +84,18 @@ async function getAllFamPhotos(req, res) {
 
 async function createPhoto(req, res) {
   try {
-    const photo = await Photo.create(req.body)
     const user = await User.findByPk(res.locals.user.id)
-    await user.addPhoto(photo)
-    return res.status(200).json(photo)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    const photoData = {
+      ...req.body,
+      userId: user.id,
+    };
+    const photo = await Photo.create(photoData)
+    return res.status(201).json(photo);
   } catch (error) {
-    return res.status(500).send(error.message)
+    return res.status(500).send(error.message);
   }
 }
 
